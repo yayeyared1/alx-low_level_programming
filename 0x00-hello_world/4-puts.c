d23be6d730c6ff8741b09c328bf165b93fd9a579
Yared Nigatu <yaredyitbarek11@gmail.com>
2022-06-30 13:12:06 -0700

    int main

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,13 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main-pints the size of various types
+ * Return: 0 if exited properly,non-zero otherwise
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
 
