bcb86107dce9bef9faa826d77b4972244b82cac7
Yared Nigatu <yaredyitbarek11@gmail.com>
2022-06-30 13:50:41 -0700

    sdfsd

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-include <stdio.h>
+#include <stdio.h>
 /**
- *  * main-pints the size of various types
- *   * Return: 0 if exited properly,non-zero 
- *   */
+*main-prints the size of various types
+*Return: 0 if exited properly, non-zero otherwise
+*/
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n"); 
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
