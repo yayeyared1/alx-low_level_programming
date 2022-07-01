cedb450da4fa614d36ba598f2cd7794867089f6e
Yared Nigatu <yaredyitbarek11@gmail.com>
2022-07-01 14:42:57 -0700

    fsdf

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 
-/** 
+/**
  *main -> assign to the variable n each time it is excuted and prints out
  *fsdfs
  *Return: always 0
@@ -16,8 +16,8 @@ for (ch = 97; ch <= 122; ch++)
 putchar(ch);
 }
 for (ch = 65; ch <= 90; ch++)
-{                                                                                                           
-putchar(ch); 
+{
+putchar(ch);
 }
 putchar(10);
 return (0);
