1b812d5871cebf7f1785514c961ca66bb2bfcc87
Yared Nigatu <yaredyitbarek11@gmail.com>
2022-07-01 14:39:09 -0700

    dsfsdf

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/** 
+ *main -> assign to the variable n each time it is excuted and prints out
+ *fsdfs
+ *Return: always 0
+*/
+
+int main(void)
+{
+int ch;
+for (ch = 97; ch <= 122; ch++)
+{
+putchar(ch);
+}
+for (ch = 65; ch <= 90; ch++)
+{                                                                                                           
+putchar(ch); 
+}
+putchar(10);
+return (0);
+}
