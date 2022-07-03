2b4bf9f7862c65bb3feddf5bc538e27bdca34d6e
Yared Nigatu <yaredyitbarek11@gmail.com>
2022-07-03 04:56:59 -0700

    ttf

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 
 /**
-*main -> Prints the last digit of a randomly generated
-*dsfsdfsd
+*main - Prints all single digit numbers of base 10 starting from 0. 
+*
 *Return: Always 0
 */
-
 int main(void)
 {
 int n;
 
 for (n = 0; n < 10; n++)
-{
 printf('%d\n', n);
-}
+
 return (0);
 }
