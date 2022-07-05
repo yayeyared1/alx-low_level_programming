fcde9e0b6f857709480b82eb3a81a735864c13b8
Yared Nigatu <yaredyitbarek11@gmail.com>
2022-07-05 10:26:31 -0700

    dfgd

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+*_islower - checkes if cha is lower case
+*@c: a chaacte
+*Return: 1 and 0 depending on condconditiondd
+*/
+
+int _islower(int c)
+{
+return (c >= 'a' && c <= 'z');
+}
