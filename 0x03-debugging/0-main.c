1bb91f81c8497a143a8a42909bd9418538bb751e
Yared Nigatu <yaredyitbarek11@gmail.com>
2022-07-06 23:38:58 -0700

    sdfsd

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
-*main - tests function that prints if integer is positive or negative
-*description
-*Return: 0
+* main - tests function that prints if integer is positive or negative
+* Return: 0
 */
 
 int main(void)
@@ -11,7 +10,7 @@ int main(void)
 int i;
 
 i = 0;
-
 positive_or_negative(i);
+
 return (0);
 }
