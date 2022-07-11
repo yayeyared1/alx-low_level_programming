216b4b453e22504a619c5fc53a4038dd979cc6f0
Yared Nigatu <yaredyitbarek11@gmail.com>
2022-07-11 04:21:19 -0700

    asdf

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,26 +8,18 @@
 */
 void rev_string(char *s)
 {
-int i, c, k;
-char *a, aux;
+char tmp;
+int i, length1, length2;
 
-a = s;
+while (s[length1] != '\0')
+length1++;
 
-while (s[c] != '\0')
+length2 = length1 - 1;
+for (i = 0; i < length1 / 2; i++)
 {
-c++;
-}
-
-for (k = 1; k < c; k++)
-{
-a++;
-}
-
-for (i = 0; i < (c / 2); i++)
-{
-aux = s[i];
-s[i] = *a;
-*a = aux;
-a--;
+tmp = s[i];
+s[i] = s[length2];
+s[length2] = tmp;
+length2 -= 1;
 }
 }
