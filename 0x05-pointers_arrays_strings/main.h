7ac7c7c6ef7244a5903e7ac4f64fd7496b231db1
Yared Nigatu <yaredyitbarek11@gmail.com>
2022-07-11 05:09:47 -0700

    asdfasd

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,4 @@
-#ifndef MAIN_H
-#define MAIN_H
-int _putchar(char c)
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -12,4 +10,3 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-#endif
